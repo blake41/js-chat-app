781a536e889b795816891caaf7226e86bd5d6dc4
Blake Johnson <blake41@@gmail.com>
2012-12-05 11:23:04 -0500

    first part of refactor good and working, need to remove redis from the translate module

--- chatServer.js
+++ chatServer.js
@@ -29,43 +29,53 @@ var users = {}
 
 io.sockets.on('connection', function (client) {
 // 1. the html form sends a message via the socket and here's our callback
-    client.on('message', function(msg) {
-      if(msg.type == "setUsername"){
-        users[msg.user] = {
-          clientId : client.id,
-          clientIP : client.handshake.address.address
-        }
-        redis.sadd("onlineUsers",msg.user);
-        client.broadcast.emit("updateBuddyList", msg.user)
-        redis.smembers("onlineUsers", function(err, members) {
-          client.emit("newBuddyList", members)        
-        })
+  client.on('message', function(msg) {
+    if(msg.type == "setUsername"){
+      users[msg.user] = {
+        clientId : client.id,
+        clientIP : client.handshake.address.address
       }
-    });
-    client.on("privateMessage", function(message) {
-      var socketId = users[message.to]['clientId'],
-          senderIP = users[message.to]['clientIP'];
+      redis.sadd("onlineUsers",msg.user);
+      client.broadcast.emit("updateBuddyList", msg.user)
+      redis.smembers("onlineUsers", function(err, members) {
+        client.emit("newBuddyList", members)        
+      })
+    }
+  });
 
-      translator.translate(message.text, function(translatedText) {
+  client.on("privateMessage", function(message) {
+    var socketId = users[message.to]['clientId'],
+        senderIP = users[message.to]['clientIP'],
+        // set default so expiresOn - Time.now < 0 and we need a new token 
+        expiresOn = 0
+    
+    redis.hgetall("token", function(err, reply) {
+      if(reply) {
+        var expiresOn = reply.expiresOn,
+        accessToken = reply.accessToken
+      }
+      translator.translate(message.text, expiresOn, accessToken, redis, function(translatedText) {
         client.set("name", message.from)
         message.text = translatedText
         io.sockets.socket(socketId).emit("privateMessage", message)
-        var formattedMessage = message.text.replace(/ /g, "_"),
-          encodedMessage = escape(message.text),
-          url = "http://" + senderIP + ":4567/say/" + encodedMessage + "";
-        
-        // need to figure out what encoding i'm getting back from bing and fix the escaping
-        request.get({
-          url : url,
-        }, function(error) {
-          console.log(error);
-        })   
+        // var formattedMessage = message.text.replace(/ /g, "_"),
+        //   encodedMessage = escape(message.text),
+        //   url = "http://" + senderIP + ":4567/say/" + encodedMessage + "",
+        //   getOptions = {
+        //     url : url,
+        //   }
+        // // need to figure out what encoding i'm getting back from bing and fix the escaping
+        // request.get(getOptions, function(error) {
+        //   console.log(error);
+        // })
       })
- 
     })
-    client.on('disconnect', function() {
-      client.get('name', function(err, name) {
-        redis.srem("onlineUsers", name)
-      })
-    });
+
+  })
+
+  client.on('disconnect', function() {
+    client.get('name', function(err, name) {
+      redis.srem("onlineUsers", name)
+    })
+  });
 });
--- translate.js
+++ translate.js
@@ -1,7 +1,8 @@
-var translate = function(toTranslate, accessToken) {
-	
-	var translatedText,
-			from = "en",
+var request = require('request'),
+  	qs = require('querystring');
+
+var makeRequest = function(toTranslate, accessToken, callback) {
+	var from = "en",
 			to = "es",
 			path = ["/V2/Ajax.svc/Translate?text=", toTranslate,
 		  				"&from=", from,
@@ -15,47 +16,37 @@ var translate = function(toTranslate, accessToken) {
 			}
 
 	request.get(options, function(err, res, body) {
-		translatedText = body;
+		callback(body);
 	})
-	return translatedText;
 }
-module.exports.translate = function (toTranslate, callback) {
-	var request = require('request'),
-		qs = require('querystring'),
-		postBody = qs.stringify({
-			client_secret : "n0ZNqCAkaGNiiDu11EUH90MXmEs5A4+QQM++RM3U1Wk=",
-			client_id : "blakeTranslate",
-			grant_type : "client_credentials",
-			scope : "http://api.microsofttranslator.com"
-		}),
-		translatedText,
-		accessToken;
-	var redis = require('redis').createClient()
-	redis.hgetall("token", function(err, reply) {
+module.exports.translate = function (toTranslate, expiresOn, accessToken, redis, callback) {
+	
+	var postBody = qs.stringify({
+  	client_secret : "n0ZNqCAkaGNiiDu11EUH90MXmEs5A4+QQM++RM3U1Wk=",
+	  client_id : "blakeTranslate",
+	  grant_type : "client_credentials",
+	  scope : "http://api.microsofttranslator.com"
+	}),
+		nowInSeconds = Math.round(Date.now() / 1000),
+		postOptions = {
+			url : "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13",
+			body : postBody
+		}
 
-		var nowInSeconds = Math.round(Date.now() / 1000),
-			expiresOn = reply.expiresOn,
-			accessToken = reply.accessToken
-			postOptions = {
-				url : "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13",
-				body : postBody
-			}
-		if((expiresOn - nowInSeconds) > 0) {
-			translatedText = translate(toTranslate, accessToken)
-		}
-		else {
-			console.log('invalid token')
-			request.post(postOptions, function(e, r, body) {
-				var tokenHash = JSON.parse(body);
-				var expiresOn = qs.parse(tokenHash.access_token).ExpiresOn;
-				var accessToken = tokenHash.access_token
-				client.hmset("token", "accessToken", accessToken, "expiresOn", expiresOn, function() {
-					translatedText = translate(toTranslate, accessToken)
-				})
+	if((expiresOn - nowInSeconds) > 0) {
+		makeRequest(toTranslate, accessToken, callback)
+	}
+	else {
+		console.log('invalid token')
+		request.post(postOptions, function(e, r, body) {
+			var tokenHash = JSON.parse(body);
+			var expiresOn = qs.parse(tokenHash.access_token).ExpiresOn;
+			var accessToken = tokenHash.access_token
+			redis.hmset("token", "accessToken", accessToken, "expiresOn", expiresOn, function() {
+				makeRequest(toTranslate, accessToken, callback)
 			})
-		}
-		callback("translated")
-	})
+		})
+	}
 }
 // separate the translation from redis, pass in whatever we need for a translation
 // args = word, token, callback
